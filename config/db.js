6bbcdc49d5f81895da0cae43aad86546313b0bc8
AkintolaDavid <akintoladvaid66@gmail.com>
2024-10-16 04:40:04 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
+const MONGO_URI = process.env.MONGO_URI;
 mongoose
-  .connect(process.env.MONGO_URI, {
-    // No need to include useNewUrlParser or useUnifiedTopology
-  })
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB Atlas");
   })
   .catch((error) => {
-    console.error("Connection error:", error);
+    console.error("Error connecting to MongoDB Atlas:", error);
   });
