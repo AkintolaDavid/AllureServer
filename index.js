e829e294ccb4cc19ac691b660cdc04a73be5fbb2
AkintolaDavid <akintoladvaid66@gmail.com>
2024-10-17 15:40:30 +0100

    h

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const db = mongojs("allureDB", ["products", "users", "customizes", "otps"]);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
 
 app.use(
   cors({
